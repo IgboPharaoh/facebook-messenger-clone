b87a6f200375d4b437ef0a8627d04cfa71f98094
IgboPharaoh <solomoneze62@gmail.com>
2022-03-13 12:00:22 +0100

    initialized state for sending messages easily

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import { Button } from "@mui/material";
+import React, {useState} from "react";
+import  './App.css'
 
 function App() {
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState(['hello', 'world'])
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMesssage = (e) => {
+    e.preventDefault()
+    setMessages([...messages, input])
+    setInput('')
+  }
+  
   return (
     <div className="App">
-      <h1>Hello Clever Programmers</h1>
-      <input />
-      <button>Send a message</button>
+      <h1>Clever Programmers</h1>
+      <form >
+        <input value={input} onChange={(e) =>setInput(e.target.value)}/>
+        <Button variant='contained' color='primary' type="submit" onClick={sendMesssage}>Send Message</Button>
+      </form>
+      {
+        messages.map((message) =>(
+          <p>{message}</p>
+        ))
+      }
     </div>
   );
 }
 
 export default App;
+
+
