821a24d6ccc2cf5ca9e8f478b68d49b48f87af8e
IgboPharaoh <solomoneze62@gmail.com>
2022-03-13 09:57:09 +0100

    setup react files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Clever Programmers</h1>
+      <input />
+      <button>Send a message</button>
+    </div>
+  );
+}
+
+export default App;
